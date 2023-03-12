09e406a9a45a88e5efa75f3488cdba2c952a7352
Fathzer <jm-dev@astesana.net>
2023-03-12 19:11:03 +0100

    Changes in core api

--- src/main/java/com/fathzer/jdbbackup/managers/dropbox/DropboxManager.java
+++ src/main/java/com/fathzer/jdbbackup/managers/dropbox/DropboxManager.java
@@ -89,7 +89,7 @@ public class DropboxManager extends DropboxBase implements DestinationManager<Dr
 	}
 
 	@Override
-	public String getProtocol() {
+	public String getScheme() {
 		return "dropbox";
 	}
 }
