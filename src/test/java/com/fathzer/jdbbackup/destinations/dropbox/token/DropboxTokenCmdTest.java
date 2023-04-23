427d266355931cecb04a998561abcf8171453756
Fathzer <jm-dev@astesana.net>
2023-04-23 10:36:28 +0200

    Adds tests of DropboxTokenCmd.getToken

--- src/test/java/com/fathzer/jdbbackup/destinations/dropbox/token/DropboxTokenCmdTest.java
+++ src/test/java/com/fathzer/jdbbackup/destinations/dropbox/token/DropboxTokenCmdTest.java
@@ -3,20 +3,21 @@ package com.fathzer.jdbbackup.destinations.dropbox.token;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mockConstruction;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Field;
 import java.net.InetSocketAddress;
 import java.net.PasswordAuthentication;
 import java.net.Proxy;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.MockedConstruction;
 
+import com.dropbox.core.DbxAuthFinish;
 import com.dropbox.core.DbxException;
-import com.fathzer.jdbbackup.JDbBackup;
+import com.dropbox.core.DbxWebAuth;
 import com.fathzer.jdbbackup.destinations.dropbox.DropboxBase;
 
 import picocli.CommandLine.ExitCode;
@@ -24,13 +25,16 @@ import picocli.CommandLine.ExitCode;
 class DropboxTokenCmdTest {
 	private static class Silent implements Console {
 		private final String command;
+		private List<String> out;
 		
 		Silent(String command) {
 			this.command = command;
+			this.out = new ArrayList<>();
 		}
 
 		@Override
 		public void out(String message) {
+			this.out.add(message);
 		}
 
 		@Override
@@ -93,38 +97,29 @@ class DropboxTokenCmdTest {
 		assertEquals(0, cmd.call());
 		assertEquals(Proxy.NO_PROXY, (Proxy) proxyField.get(cmd));
 		assertNull(proxyLoginField.get(cmd));
-		
-		
-/*		
-		// No proxy
-		try (MockedConstruction<DropboxTokenCmd> mock = mockConstruction(DropboxTokenCmd.class)) {
-			assertEquals(0, DropboxTokenCmd.doIt());
-			verify(mock.constructed().get(0), never()).setProxy(any(), any());
+	}
+	
+	@Test
+	void testGetToken() throws Exception {
+		DropboxTokenCmd cmd = new DropboxTokenCmd();
+		final String url = "http://dropbox/url";
+		try (MockedConstruction<DbxWebAuth> mock = mockConstruction(DbxWebAuth.class, (auth,context) -> {
+			when(auth.authorize(any())).thenReturn(": "+url);
+			when(auth.finishFromCode("ok")).thenReturn(new DbxAuthFinish("access", 10L, "refresh", "user", "team", "account", "state"));
+			when(auth.finishFromCode("ko")).thenThrow(DbxException.class);
+		})) {
+			// Test when auth is ok
+			Silent console = new Silent("ok");
+			cmd.setConsole(console);
+			cmd.getToken();
+			assertTrue(console.out.get(0).endsWith(url));
+			assertTrue(console.out.get(console.out.size()-2).endsWith(": refresh-refresh"));
+
+			// Test when auth is ko
+			console = new Silent("ko");
+			cmd.setConsole(console);
+			assertThrows(Exception.class, () -> cmd.getToken());
 		}
-		// With proxy
-		try (MockedConstruction<DropboxTokenCmd> mock = mockConstruction(DropboxTokenCmd.class)) {
-			assertEquals(0, DropboxTokenCmd.doIt("-p","host:4321"));
-			final DropboxTokenCmd cmd = mock.constructed().get(0);
-			ArgumentCaptor<Proxy> proxyCaptor = ArgumentCaptor.forClass(Proxy.class);
-			ArgumentCaptor<PasswordAuthentication> loginCaptor = ArgumentCaptor.forClass(PasswordAuthentication.class);
-			verify(cmd).setProxy(proxyCaptor.capture(), loginCaptor.capture());
-			Proxy proxy = proxyCaptor.getValue();
-			assertEquals(new InetSocketAddress("host", 4321), proxy.address());
-			assertEquals("a", loginCaptor.getValue().getUserName());
-			assertArrayEquals(new char[] {'b'}, loginCaptor.getValue().getPassword());
-		}
-		// With proxy + password
-		try (MockedConstruction<DropboxTokenCmd> mock = mockConstruction(DropboxTokenCmd.class)) {
-			assertEquals(0, DropboxTokenCmd.doIt("-p","a:b@host:1234"));
-			final DropboxTokenCmd cmd = mock.constructed().get(0);
-			ArgumentCaptor<Proxy> proxyCaptor = ArgumentCaptor.forClass(Proxy.class);
-			ArgumentCaptor<PasswordAuthentication> loginCaptor = ArgumentCaptor.forClass(PasswordAuthentication.class);
-			verify(cmd).setProxy(proxyCaptor.capture(), loginCaptor.capture());
-			Proxy proxy = proxyCaptor.getValue();
-			assertEquals(new InetSocketAddress("host", 1234), proxy.address());
-			assertEquals("a", loginCaptor.getValue().getUserName());
-			assertArrayEquals(new char[] {'b'}, loginCaptor.getValue().getPassword());
-		}*/
 	}
 
 }
