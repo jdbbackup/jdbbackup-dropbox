3127f3bd6dbd4cd87386827a36e04df28583d2dd
Fathzer <jm-dev@astesana.net>
2023-04-23 19:11:55 +0200

    More tests

--- src/test/java/com/fathzer/jdbbackup/destinations/dropbox/DropboxBaseTest.java
+++ src/test/java/com/fathzer/jdbbackup/destinations/dropbox/DropboxBaseTest.java
@@ -1,12 +1,18 @@
 package com.fathzer.jdbbackup.destinations.dropbox;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import java.net.Authenticator;
 import java.net.InetSocketAddress;
+import java.net.PasswordAuthentication;
 import java.net.Proxy;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
 
+import com.dropbox.core.http.StandardHttpRequestor.Config;
 import com.fathzer.plugin.loader.utils.ProxySettings;
 
 class DropboxBaseTest {
@@ -17,6 +23,30 @@ class DropboxBaseTest {
 		assertNull(base.getProxyAuthentication());
 		base.setProxy(ProxySettings.fromString("host:3128").toProxy(), null);
 		assertEquals(new InetSocketAddress("host",3128), base.getProxy().address());
+		
+		// Test proxy is set
+		{
+			final Config.Builder builder = mock(Config.Builder.class);
+			try (MockedStatic<Config> config = mockStatic(Config.class)) {
+				config.when(Config::builder).thenReturn(builder);
+				ArgumentCaptor<Proxy> captor = ArgumentCaptor.forClass(Proxy.class);
+				assertNotNull(base.getConfig());
+				verify(builder).withProxy(captor.capture());
+				assertEquals(new InetSocketAddress("host",3128), captor.getValue().address());
+				
+				assertNull(Authenticator.getDefault());
+			}
+		}
+
+		// Test proxy auth is set
+		ProxySettings settings = ProxySettings.fromString("a:b@host:3128");
+		base.setProxy(settings.toProxy(), settings.getLogin());
+		assertNotNull(base.getConfig());
+		PasswordAuthentication login = Authenticator.getDefault().requestPasswordAuthenticationInstance(null, null, 0, null, null, null, null, null);
+		assertEquals("a", login.getUserName());
+		assertArrayEquals(new char[]{'b'}, login.getPassword());
+
+		
 		base.setProxy(Proxy.NO_PROXY, null);
 		assertEquals(Proxy.NO_PROXY, base.getProxy());
 		
@@ -24,5 +54,12 @@ class DropboxBaseTest {
 		assertThrows(RuntimeException.class, () -> base.getAppInfo());
 		base.setDbxAppInfoSupplier(() -> DropboxBase.RESOURCE_PROPERTY_APP_INFO_BUILDER.apply("wrongAppFile2.properties"));
 		assertThrows(RuntimeException.class, () -> base.getAppInfo());
+		
+		
+		assertThrows(IllegalArgumentException.class, () -> base.setProxy(null, null));
+		final PasswordAuthentication auth = new PasswordAuthentication("a", new char[0]);
+		assertThrows(IllegalArgumentException.class, () -> base.setProxy(Proxy.NO_PROXY, auth));
+
+		
 	}
 }
